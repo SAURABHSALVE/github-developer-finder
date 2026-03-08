bd364185425e475a3c7575964aada2e6a5b6178e
SAURABHSALVE <saurabhsalve9999@gmail.com>
2026-03-08 17:44:45 +0530

    Initial commit: Complete Angular GitHub Developer Finder app

--- Downloads/github-developer-finder/src/app/app.component.ts
+++ Downloads/github-developer-finder/src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { RepoListComponent } from './components/repo-list/repo-list.component';
+import { GithubService } from './services/github.service';
+import { User } from './models/user.model';
+import { Repo } from './models/repo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SearchBarComponent, UserProfileComponent, RepoListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'github-developer-finder';
+  user: User | null = null;
+  repos: Repo[] = [];
+  errorMessage: string = '';
+
+  constructor(private githubService: GithubService) {}
+
+  onSearch(username: string) {
+    this.errorMessage = '';
+    this.githubService.getUser(username).subscribe({
+      next: (user) => {
+        this.user = user;
+        this.githubService.getRepos(username).subscribe({
+          next: (repos) => {
+            this.repos = repos;
+          },
+          error: (err) => {
+            this.errorMessage = err.message;
+          }
+        });
+      },
+      error: (err) => {
+        this.errorMessage = err.message;
+        this.user = null;
+        this.repos = [];
+      }
+    });
+  }
 }
--- Downloads/github-developer-finder/src/app/components/user-profile/user-profile.component.ts
+++ Downloads/github-developer-finder/src/app/components/user-profile/user-profile.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-
+  @Input() user!: User;
 }
